3c7ad344c886d1555b327de87947e82ab112abdc
Matt Alioto <matthew.j.alioto@gmail.com>
2024-08-10 03:50:43 -0400

    Support running DemolitionSpider multiple times with different arguments

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from scrapy.crawler import CrawlerProcess
 from src.demolition_spider import DemolitionSpider, PermitType
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
 
-process = CrawlerProcess()
-process.crawl(DemolitionSpider, permit_type=PermitType.residential_wrecking_permit, start_date='08/01/2023')
-process.start()
+settings = get_project_settings()
+configure_logging(settings)
+runner = CrawlerRunner(settings)
+
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(
+        DemolitionSpider,
+        permit_type=PermitType.commercial_wrecking_permit,
+        start_date="08/01/2023",
+    )
+    yield runner.crawl(
+        DemolitionSpider,
+        permit_type=PermitType.residential_wrecking_permit,
+        start_date="08/01/2023",
+    )
+    reactor.stop()
+
+
+crawl()
+reactor.run()
